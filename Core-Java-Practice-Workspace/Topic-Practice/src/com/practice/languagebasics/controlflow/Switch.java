00d2e2b1a76d0e202cc8184b366ff3937b472552
Ian Perfitt <iancp@protonnmail.com>
2024-03-27 18:04:10 -0500

    first commit

---
+++ Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/languagebasics/controlflow/Switch.java
@@ -0,0 +1,25 @@
+package com.practice.languagebasics.controlflow;
+
+public class Switch {
+
+	public static void main(String[] args) {
+
+		int i = 1;
+
+		switch (i) {
+		case 0: case 1:
+			System.out.println("0");
+		case 2:
+			System.out.println("2");
+			
+		case 3:
+			System.out.println("3");
+			break;
+		default:
+			System.out.println("default");
+
+		}
+
+	}
+
+}
---
+++ Hibernate-Practice-Workspace/.metadata/.plugins/org.eclipse.core.resources/.history/31/90213e5444d7001e1710e42abc72d6ac
@@ -0,0 +1,8 @@
+package com.example.demo;
+
+public class ThingOne {
+
+	public ThingOne(ThingTwo thingTwo, ThingThree thingThree) {
+		// ...
+	}
+}
