530deab3ed18ee446874cd8a9bcb49552eb8e3ce
Ian Perfitt <ianperfitt@gmail.com>
2024-04-12 16:59:11 -0500

    renamed files and added new map class for practicing

--- Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/collections/interfaces/implementations/list/ArrayListDemo.java
+++ Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/collections/interfaces/implementations/list/ArrayListDemo.java
@@ -1,4 +1,4 @@
-package com.practice.collections.interfaces.implementations;
+package com.practice.collections.interfaces.implementations.list;
 
 import java.util.ArrayList;
 import java.util.List;
--- Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/collections/interfaces/implementations/set/HashSetDemo.java
+++ Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/collections/interfaces/implementations/set/HashSetDemo.java
@@ -1,4 +1,4 @@
-package com.practice.collections.interfaces.implementations;
+package com.practice.collections.interfaces.implementations.set;
 
 import java.util.HashSet;
 import java.util.Set;
