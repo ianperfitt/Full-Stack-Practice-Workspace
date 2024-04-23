640d215ac7d248481ad5f63259340bf358aab7fb
Ian Perfitt <iancp@protonnmail.com>
2024-04-22 20:00:36 -0500

    created classess for practicing synchronized keyword

---
+++ Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/threads/synchronized_keyword/SyncDemo.java
@@ -0,0 +1,17 @@
+package com.practice.threads.synchronized_keyword;
+
+class Counter {
+	int count;
+	
+	public void increment() {
+		count++;
+	}
+}
+
+public class SyncDemo {
+	
+	public static void main(String[] args) {
+		
+	}
+
+}
