71ec790df19f79f386bc38e98f73c787619dcd4d
Ian Perfitt <iancp@protonnmail.com>
2024-04-27 17:39:33 -0500

    added Junit test suite

--- Core-Java-Practice-Workspace/LeetCode-Practice/src/Solution.java
+++ Core-Java-Practice-Workspace/LeetCode-Practice/src/Solution.java
@@ -1,26 +1,8 @@
-import java.util.HashMap;
-import java.util.Map;
-
 public class Solution {
 
-	public static int numberOfSubarrays(int[] nums, int k) {
-		Map<Integer, Integer> counts = new HashMap<>();
-		counts.put(0, 1);
+	public int maxNumberOfBalloons(String text) {
 
-		int ans = 0;
-		int curr = 0;
+		return 0;
 
-		for (int num : nums) {
-			curr += num % 2;
-			ans += counts.getOrDefault(curr - k, 0);
-			counts.put(curr, counts.getOrDefault(curr, 0) + 1);
-		}
-		return ans;
-	}
-
-	public static void main(String[] args) {
-
-		int[] arr = { 1, 1, 2, 1, 1 };
-		System.out.println(numberOfSubarrays(arr, 3));
 	}
 }
--- Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/unittesting/SimpleCalculator.java
+++ Core-Java-Practice-Workspace/Topic-Practice/src/com/practice/unittesting/SimpleCalculator.java
@@ -8,4 +8,11 @@ public class SimpleCalculator {
 		}
 		return numberA + numberB;
 	}
+
+	public static void main(String[] args) {
+		String s1 = "hello";
+		String s2 = "hello";
+		
+		System.out.println(s1 == s2);
+	}
 }
