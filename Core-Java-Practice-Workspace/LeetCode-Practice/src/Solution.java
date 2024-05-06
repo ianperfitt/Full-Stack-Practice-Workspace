a09c4718c29682de5a80fed4a18628064227bec8
Ian Perfitt <iancp@protonnmail.com>
2024-05-06 17:34:11 -0500

    working on solution for LeetCode challenge

--- Core-Java-Practice-Workspace/LeetCode-Practice/src/Solution.java
+++ Core-Java-Practice-Workspace/LeetCode-Practice/src/Solution.java
@@ -2,22 +2,26 @@ import java.util.HashMap;
 import java.util.Map;
 
 class Solution {
-	public boolean canConstruct(String ransomNote, String magazine) {
+	public int lengthOfLongestSubstring(String s) {
 
-		Map<Character, Integer> map = new HashMap<>();
+		Map<Character, Integer> tracker = new HashMap<>();
+
+		char c = 'a';
 		int count = 0;
+		int solution = 0;
 
-		for (char c : magazine.toCharArray()) {
-			if (map.containsKey(c)) {
-				count = map.get(c) - 1;
-				if (count == 0) {
-					map.remove(c);
-				} else {
-					map.put(c, count);
-				}
+		for (int i = 0; i < s.length(); i++) {
+			c = s.charAt(i);
+
+			if (!tracker.containsKey(c)) {
+				tracker.put(c, tracker.getOrDefault(c, 0) + 1);
+			} else {
+				tracker.put(c, tracker.get(c) + 1);
+				solution = Math.max(count, count - tracker.get(c) + 1);
 			}
-
+			++count;
 		}
-		return map.isEmpty();
+
+		return 0;
 	}
 }
